8194fd7ec11f9696689afd76cf8f4b6f175c20f2
hakantopcu1989 <89578374+hakantopcu1989@users.noreply.github.com>
2023-06-17 13:19:14 +0300

    Add files via upload

---
+++ ForLoop/Program.cs
@@ -0,0 +1,51 @@
+﻿ 
+using System;
+
+ namespace ForLoop
+ {
+     class Program
+{
+    static void Main(string[] args)
+    {
+        //ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır
+        Console.Write("Lütfen Bir sayı giriniz: ");
+        int sayac=int.Parse(Console.ReadLine());
+        for (int i = 0; i < sayac; i++)
+        {
+            if(i%2==1)
+            Console.WriteLine(i);
+           //komutlar
+        }
+
+        // 1 ile 1000 arasındaki çift sayıların kendi içlerindeki toplamlar
+        int tekToplam=0;
+        int ciftToplam=0;
+        for (int i = 1; i <=1000 ; i++)
+        {
+            if(i%2==1)
+            tekToplam+=i; //tektoplam=tektoplam+i
+            else
+            ciftToplam+=i; 
+        }
+        Console.WriteLine("Tek Toplam: " + tekToplam);
+        Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+        // break,continue
+
+        for (int i = 0; i < 10; i++)
+        {
+            if(i==4)
+            break;
+            Console.WriteLine(i);
+        }
+        
+          for (int i = 0; i < 10; i++)
+        {
+            if(i==4)
+            continue;
+            Console.WriteLine(i);
+        }
+
+    }
+}
+ }
